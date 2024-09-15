32299a5e7f0f6ee26963c6d62b9f9f0048b49751
Satya viswa pavan Ranga <pavan.sv.ranga@gmail.com>
2024-09-14 20:21:45 -0400

    Remove trash folder from GitHub

--- trash/trash-v4/segment_display_service.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def display_segments(segments):
-    for segment in segments:
-        st.write(f"Time: {segment['start']:.2f}s - {segment['end']:.2f}s")
-        st.write(f"Text: {segment['text']}")
-        st.write("---")
